06976704398934fdf26f4c7dc09702866333fcb2
MirabdullaYuldashev <mirabdullayuldashev@gmail.com>
2023-04-08 10:22:35 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Timer from "./components/Time";
+
+const App = () => {
+    return (
+        <div>
+            <Timer/>
+        </div>
+    )
+}
+export default App;
